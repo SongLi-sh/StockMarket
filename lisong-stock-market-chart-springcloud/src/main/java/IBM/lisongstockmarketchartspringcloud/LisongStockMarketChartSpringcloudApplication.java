d1e5361574895f032d3429ab909f33494e20e19e
SongLi-sh <zte_ls@126.com>
2020-05-05 19:38:12 +0800

    add pom.xml and file at src/

---
+++ lisong-stock-market-chart-springcloud/src/main/java/IBM/lisongstockmarketchartspringcloud/LisongStockMarketChartSpringcloudApplication.java
@@ -0,0 +1,13 @@
+package IBM.lisongstockmarketchartspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LisongStockMarketChartSpringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LisongStockMarketChartSpringcloudApplication.class, args);
+	}
+
+}
---
+++ lisong-stock-market-chart-springcloud/src/test/java/IBM/lisongstockmarketchartspringcloud/LisongStockMarketChartSpringcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package IBM.lisongstockmarketchartspringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LisongStockMarketChartSpringcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
